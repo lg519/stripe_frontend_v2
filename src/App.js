e3a351cdf6027f3ed5d4e0c445241929429d9518
lg519 <lg519@ic.ac.uk>
2022-12-24 10:22:38 +0000

    working frontend v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import StripeCheckout from "react-stripe-checkout";
+
+//require("dotenv").config();
 
 function App() {
+  const [product, setProduct] = useState({
+    name: "KALLISTON plan",
+    price: 1,
+    productBy: "KALLISTON",
+  });
+
+  const makepayment = (token) => {
+    const body = {
+      token,
+      product,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body),
+    })
+      .then((res) => {
+        console.log("RESPONSE:", res);
+        const { status } = res;
+        console.log("STATUS:", status);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +50,14 @@ function App() {
         >
           Learn React
         </a>
+        <StripeCheckout
+          stripeKey={
+            "pk_live_51MGkyqCQWfdwqcJKrM5Mh9r22l0nHjyXrM6N2DqDEjXr5bWrmRwdC5MssthGZFFEqjrFshOlPXBDPwijtfHhjTHA00KRCUg2Xp"
+          }
+          token={makepayment}
+          name="Buy KALLISTON plan"
+          amount={product.price * 100}
+        ></StripeCheckout>
       </header>
     </div>
   );
